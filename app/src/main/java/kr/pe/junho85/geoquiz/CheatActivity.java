a75ccc0ba2f6a2f2a43bd4d5d2ff861a2bc9070d
june.kim <junho85@gmail.com>
2017-01-12 23:14:01 +0900

    first commit

---
+++ app/src/main/java/kr/pe/junho85/geoquiz/CheatActivity.java
@@ -0,0 +1,13 @@
+package kr.pe.junho85.geoquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CheatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+    }
+}
